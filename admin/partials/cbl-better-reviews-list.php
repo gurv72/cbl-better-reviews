2c43b02267d5694d59616478f6b4185d5b34a737
gurv72 <91877244+gurv72@users.noreply.github.com>
2022-01-05 10:43:35 +0000

    Updates

--- admin/partials/cbl-better-reviews-list.php
+++ admin/partials/cbl-better-reviews-list.php
@@ -18,9 +18,9 @@
 
 	<?php } ?>
 
-	</p>
-
 	<input type='button' value='Remove' onclick=remove_field('<?php echo $type; ?>_input_text<?php echo $counter; ?>');>
+
+	</p>
 
 <?php } ?>
 
@@ -32,10 +32,47 @@
 function <?php echo $type; ?>_add_field() {
 	var total_text=document.getElementsByClassName("<?php echo $type; ?>_input_text");
 	total_text=total_text.length+1;
-	document.getElementById("<?php echo $type; ?>_field_div").innerHTML=document.getElementById("<?php echo $type; ?>_field_div").innerHTML+"<p id='<?php echo $type; ?>_input_text"+total_text+"_wrapper'><input type='text' name='<?php echo $section_name; ?>[subtype]["+total_text+"][<?php echo $type; ?>_subtype]' class='<?php echo $type; ?>_input_text' id='<?php echo $type; ?>_input_text_subtype"+total_text+"' placeholder='Subtype' value=''><input type='text' name='<?php echo $section_name; ?>[subtype]["+total_text+"][<?php echo $type; ?>_subtype_name]' class='<?php echo $type; ?>_input_text' id='<?php echo $type; ?>input_text_name"+total_text+"' placeholder='Subtype Name'><input type='text' name='<?php echo $section_name; ?>[subtype]["+total_text+"][<?php echo $type; ?>_subtype_text]' class='<?php echo $type; ?>_input_text' id='<?php echo $type; ?>_input_text_description"+total_text+"_description' placeholder='Subtype Description'><input type='button' value='Remove' onclick=remove_field('<?php echo $type; ?>_input_text"+total_text+"');></p>";
+
+	var parent = document.getElementById("<?php echo $type; ?>_field_div");
+	const p = document.createElement('p');
+	p.setAttribute("id", "<?php echo $type; ?>_input_text"+total_text+"_wrapper");
+	parent.appendChild(p);
+
+	const text1 = document.createElement('input');
+	text1.setAttribute("type", "text");
+	text1.setAttribute("name", "<?php echo $section_name; ?>[subtype]["+total_text+"][<?php echo $type; ?>_subtype]");
+	text1.setAttribute("class", "<?php echo $type; ?>_input_text");
+	text1.setAttribute("placeholder", "Subtype");
+
+	const text2 = document.createElement('input');
+	text2.setAttribute("type", "text");
+	text2.setAttribute("name", "<?php echo $section_name; ?>[subtype]["+total_text+"][<?php echo $type; ?>_subtype_name]");
+	text2.setAttribute("class", "<?php echo $type; ?>_input_text");
+	text2.setAttribute("placeholder", "Subtype");
+
+	const text3 = document.createElement('input');
+	text3.setAttribute("type", "text");
+	text3.setAttribute("name", "<?php echo $section_name; ?>[subtype]["+total_text+"][<?php echo $type; ?>_subtype_text]");
+	text3.setAttribute("class", "<?php echo $type; ?>_input_text");
+	text3.setAttribute("placeholder", "Subtype");
+
+	const button = document.createElement('BUTTON');
+	var button_text = document.createTextNode("Remove");
+  	button.appendChild(button_text);
+	button.setAttribute("onclick", "remove_field('<?php echo $type; ?>_input_text"+total_text+"');");
+
+	p.appendChild(text1);
+	p.appendChild(text2);
+	p.appendChild(text3);
+	p.appendChild(button);
+
+	//const text1 = document.createElement('input');
+
+	//document.getElementById("<?php echo $type; ?>_field_div").innerHTML=document.getElementById("<?php echo $type; ?>_field_div").innerHTML+"<p id='<?php echo $type; ?>_input_text"+total_text+"_wrapper'><input type='text' name='<?php echo $section_name; ?>[subtype]["+total_text+"][<?php echo $type; ?>_subtype]' class='<?php echo $type; ?>_input_text' id='<?php echo $type; ?>_input_text_subtype"+total_text+"' placeholder='Subtype' value=''><input type='text' name='<?php echo $section_name; ?>[subtype]["+total_text+"][<?php echo $type; ?>_subtype_name]' class='<?php echo $type; ?>_input_text' id='<?php echo $type; ?>input_text_name"+total_text+"' placeholder='Subtype Name'><input type='text' name='<?php echo $section_name; ?>[subtype]["+total_text+"][<?php echo $type; ?>_subtype_text]' class='<?php echo $type; ?>_input_text' id='<?php echo $type; ?>_input_text_description"+total_text+"_description' placeholder='Subtype Description'><input type='button' value='Remove' onclick=remove_field('<?php echo $type; ?>_input_text"+total_text+"');></p>";
 }
 
 function remove_field(id) {
+
 	document.getElementById(id+"_wrapper").outerHTML="";
 }
 
--- admin/partials/cbl-better-reviews-text.php
+++ admin/partials/cbl-better-reviews-text.php
@@ -1,4 +1,4 @@
 <div style="padding-bottom:20px;">
 <label><?php echo $field_name; ?></label><br />
-<input type="text" id="<?php echo $field_value; ?>" name="<?php echo $section_name; ?>[<?php echo $field_value; ?>]" value="<?php echo $field_text; ?>" placeholder="<?php echo $field_name; ?>">
+<input type="text" id="<?php echo $field_value; ?>" name="<?php echo $section_name; ?>[<?php echo $field_value; ?>]" value="<?php if (!empty($field_text)) { echo $field_text; } ?>" placeholder="<?php echo $field_name; ?>">
 </div>
