f688490285844b87951b8aa49194ff539b9d236e
gurv72 <91877244+gurv72@users.noreply.github.com>
2022-01-05 11:14:11 +0000

    Fixed the javascript

--- admin/partials/cbl-better-reviews-list.php
+++ admin/partials/cbl-better-reviews-list.php
@@ -65,10 +65,6 @@ function <?php echo $type; ?>_add_field() {
 	p.appendChild(text2);
 	p.appendChild(text3);
 	p.appendChild(button);
-
-	//const text1 = document.createElement('input');
-
-	//document.getElementById("<?php echo $type; ?>_field_div").innerHTML=document.getElementById("<?php echo $type; ?>_field_div").innerHTML+"<p id='<?php echo $type; ?>_input_text"+total_text+"_wrapper'><input type='text' name='<?php echo $section_name; ?>[subtype]["+total_text+"][<?php echo $type; ?>_subtype]' class='<?php echo $type; ?>_input_text' id='<?php echo $type; ?>_input_text_subtype"+total_text+"' placeholder='Subtype' value=''><input type='text' name='<?php echo $section_name; ?>[subtype]["+total_text+"][<?php echo $type; ?>_subtype_name]' class='<?php echo $type; ?>_input_text' id='<?php echo $type; ?>input_text_name"+total_text+"' placeholder='Subtype Name'><input type='text' name='<?php echo $section_name; ?>[subtype]["+total_text+"][<?php echo $type; ?>_subtype_text]' class='<?php echo $type; ?>_input_text' id='<?php echo $type; ?>_input_text_description"+total_text+"_description' placeholder='Subtype Description'><input type='button' value='Remove' onclick=remove_field('<?php echo $type; ?>_input_text"+total_text+"');></p>";
 }
 
 function remove_field(id) {
