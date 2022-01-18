e3cd1c6382debe54a07919572df73473a6036242
Gurvinder Daheley <gurvinder.daheley@manifesto.co.uk>
2022-01-18 14:51:54 +0000

    Updates

--- public/js/cbl-better-reviews-public.js
+++ public/js/cbl-better-reviews-public.js
@@ -3,6 +3,8 @@
 
 	var postId = document.getElementById('br-likes-postid');
 	var button = document.getElementById("br-likes-likebutton");
+	var likeText = document.getElementById("cbl-like-text");
+	var likedText = document.getElementById("cbl-liked-text");
 	var likeTotal = document.getElementById("br-likes-total");
 
 	var likedArray;
@@ -16,11 +18,13 @@
 	}
 
 	if (likedArray.indexOf(postId.value) !== -1) {
-		button.innerHTML = 'Unlike';
-		button.disabled = true;
+		button.style.display = 'none';
+		likeText.style.display = 'none';
+		likedText.style.display = 'block';
+	} else {
+		likeText.style.display = 'block';
+		likedText.style.display = 'none';
 	}
-
-	console.log(likedArray);
 
 	// Click the button
 	button.addEventListener("click",function(e){
@@ -39,40 +43,38 @@
 			if (this.readyState == 4 && this.status == 200) {
 
 				if (task == 'like') {
-					button.innerHTML = 'Unlike';
+					button.style.display = 'none';
+					likeText.style.display = 'none';
+					likedText.style.display = 'block';
 					update_like_total();
-					button.setAttribute('data-task', 'unlike');
 
 					if (likedArray.indexOf(postId.value) == -1) {
 						likedArray.push(postId.value);
  						localStorage.setItem('likedPosts', JSON.stringify(likedArray));
 					}
-				} /* else {
-					button.innerHTML = 'Like';
-					update_like_total();
-					button.setAttribute('data-task', 'like');
-				} */
-			}};
+				}
+			}
+		};
 
-		},false);
+	},false);
 
-		function update_like_total() {
+	function update_like_total() {
 
-			var postId = document.getElementById('br-likes-postid');
-			var likeData = 'task=likedata&post_id='+postId.value;
+		var postId = document.getElementById('br-likes-postid');
+		var likeData = 'task=likedata&post_id='+postId.value;
 
-			// This is to update the like div, will need to be changed
-			var datarequest = new XMLHttpRequest();
-			datarequest.open('POST', '/', true);
-			datarequest.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
+		// This is to update the like div, will need to be changed
+		var datarequest = new XMLHttpRequest();
+		datarequest.open('POST', '/', true);
+		datarequest.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
 
-			// Send the data
-			var datastatus = datarequest.send(likeData);
-			datarequest.onreadystatechange = function() {
-			if (this.readyState == 4 && this.status == 200) {
-				likeTotal.innerHTML = datarequest.response;
-				console.log(datarequest.response);
-			}};
-		}
+		// Send the data
+		var datastatus = datarequest.send(likeData);
+		datarequest.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
+			likeTotal.innerHTML = datarequest.response;
+			console.log(datarequest.response);
+		}};
+	}
 
 })();
