551be98939f1b619159662bc4b48e7d36d283fbe
Gurvinder Daheley <gurvinder.daheley@manifesto.co.uk>
2022-01-18 14:29:57 +0000

    Updates

--- public/js/cbl-better-reviews-public.js
+++ public/js/cbl-better-reviews-public.js
@@ -36,45 +36,43 @@
 
 		// Once the request is sent
 		request.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
+			if (this.readyState == 4 && this.status == 200) {
 
-			if (task == 'like') {
-				button.innerHTML = 'Unlike';
-				update_like_total();
-				button.setAttribute('data-task', 'unlike');
+				if (task == 'like') {
+					button.innerHTML = 'Unlike';
+					update_like_total();
+					button.setAttribute('data-task', 'unlike');
 
-				if (likedArray.indexOf(postId.value) == -1) {
-					likedArray.push(postId.value);
-  					localStorage.setItem('likedPosts', JSON.stringify(likedArray));
-				}
-			} /* else {
-				button.innerHTML = 'Like';
-				update_like_total();
-				button.setAttribute('data-task', 'like');
-			} */
+					if (likedArray.indexOf(postId.value) == -1) {
+						likedArray.push(postId.value);
+ 						localStorage.setItem('likedPosts', JSON.stringify(likedArray));
+					}
+				} /* else {
+					button.innerHTML = 'Like';
+					update_like_total();
+					button.setAttribute('data-task', 'like');
+				} */
+			}};
 
-        }};
+		},false);
 
-	},false);
+		function update_like_total() {
 
-	function update_like_total() {
+			var postId = document.getElementById('br-likes-postid');
+			var likeData = 'task=likedata&post_id='+postId.value;
 
-		var postId = document.getElementById('br-likes-postid');
-		var likeData = 'task=likedata&post_id='+postId.value;
+			// This is to update the like div, will need to be changed
+			var datarequest = new XMLHttpRequest();
+			datarequest.open('POST', '/', true);
+			datarequest.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
 
-		// This is to update the like div, will need to be changed
-		var datarequest = new XMLHttpRequest();
-		datarequest.open('POST', '/', true);
-		datarequest.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
-
-		// Send the data
-		var datastatus = datarequest.send(likeData);
-		datarequest.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-			likeTotal.innerHTML = datarequest.response;
-			console.log(datarequest.response);
-
-		}};
-	}
+			// Send the data
+			var datastatus = datarequest.send(likeData);
+			datarequest.onreadystatechange = function() {
+			if (this.readyState == 4 && this.status == 200) {
+				likeTotal.innerHTML = datarequest.response;
+				console.log(datarequest.response);
+			}};
+		}
 
 })();
